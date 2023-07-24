ee8600b5c1e4866ae270601095b2209ead0d2ac0
jennyxing <jennyxing27@gmail.com>
2023-07-24 16:09:57 -0400

    jsx cda all code

---
+++ jsx-codealong/src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Header from "./components/Header/Header";
+import StudentList from "./components/StudentList/StudentList";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <StudentList />
+    </>
+  );
+}
+
+export default App;
---
+++ jsx-codealong/src/components/StudentList/StudentList.js
@@ -0,0 +1,13 @@
+import "./StudentList.scss";
+import Student from "../Student/Student";
+const StudentList = () => {
+    return (
+        <section className="student-list">
+            <Student />
+            <Student />
+            <Student />
+        </section>
+    )
+}
+
+export default StudentList;
